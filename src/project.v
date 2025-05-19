6017fb6c90ad8aff6e920c43052e4435aa3a8ba3
sanjaycal <sanjay.calgary@gmail.com>
2025-05-19 19:52:54 -0400

    fixes maybe?

--- src/project.v
+++ src/project.v
@@ -22,7 +22,7 @@ module tt_um_counter_example (
     if (!rst_n) begin // If reset is active (low)
       counter_val <= 8'd0; // Reset counter to 0
     end else begin      // Else, on the rising edge of the clock
-      counter_val <= counter_val[7:0] + 1; // Increment counter
+      counter_val[7:0] <= counter_val[7:0] + 1; // Increment counter
     end
   end
 
