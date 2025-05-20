f8a1241ec2d5ae71f9a6da0bfff24c8a9c641371
sanjaycal <sanjay.calgary@gmail.com>
2025-05-20 12:22:11 -0400

    forgot that my enable was ui_in[0] so actually enabling my counter now

--- test/test.py
+++ test/test.py
@@ -17,7 +17,7 @@ async def test_project(dut):
     # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
-    dut.ui_in.value = 0
+    dut.ui_in.value = 1
     dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
