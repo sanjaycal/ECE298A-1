d2341b031ea21450c076ac648ff663f7805127a2
sanjaycal <sanjay.calgary@gmail.com>
2025-05-20 12:10:27 -0400

    maybe fix the tests?

--- test/test.py
+++ test/test.py
@@ -21,14 +21,15 @@ async def test_project(dut):
     dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
+    assert dut.uo_out.value == 0
     dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
 
     # Wait for one clock cycle to see the output values
     for i in range(256):
-        await ClockCycles(dut.clk, 1)
-        assert dut.uo_out.value == i - 1
+        await ClockCycles(dut.clk, 2)
+        assert dut.uo_out.value == i
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
